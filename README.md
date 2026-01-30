74ee698263460dd9f71099f31ba8ff148d5cd31c
James <systematicquant08@gmail.com>
2026-01-30 23:43:15 +1100

    created and initial working version of daily analysis tookit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # algo_trade_management
+
+daily_analysis progress: via chatgpt:
+
+301/2/2026: completed single stock charting nb, completed multiple stock + daily update to sqlite db nb.
+next step:  read the db with filter, then plot via plotly for each symbol / period, filtering is offline and only to db
---
+++ daily_analysis/daily_static_charts.py
@@ -0,0 +1,36 @@
+import yfinance as yf
+import pandas as pd
+
+import mplfinance as mpf  # extra lib for pour candle stick chart
+
+ticker = "BOE.AX"
+
+df = yf.download(
+    "BOE.AX",
+    start="2020-01-01",
+    auto_adjust=False,   # no corporate action adjustment just raw price
+    group_by="column"    # 
+)
+# above df has multi header rows
+'''
+Price      Adj Close  Close   High    Low   Open  Volume
+Ticker        BOE.AX BOE.AX BOE.AX BOE.AX BOE.AX  BOE.AX
+Date                                                    
+2020-01-02     0.392  0.392  0.408  0.392  0.408   29833
+2020-01-03     0.400  0.400  0.408  0.400  0.408    9863
+2020-01-06     0.400  0.400  0.424  0.384  0.424  104375
+2020-01-07     0.392  0.392  0.392  0.376  0.392   32908
+'''
+
+df = df.droplevel(1, axis=1) if isinstance(df.columns, pd.MultiIndex) else df  # remove second header, row
+print(df.head())
+
+
+# display chart using mplfinance
+mpf.plot(
+    df,
+    type="candle",
+    volume=True,
+    style="yahoo",
+    title="BOE.AX Price",
+)
